08f9d27c267dfb0ddca61707598de011d7e783b6
izzumani <isaac.njiru@gmail.com>
2021-07-03 05:44:26 +0000

    config changes and capybara spec

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0', '>= 4.0.1'
+  gem 'capybara'
+  gem 'capybara-screenshot'
 end
 
 group :development do
---
+++ spec/support/system.rb
@@ -0,0 +1,35 @@
+require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
+
+RSpec.configure do |config|
+    config.before(:each, type: :system) do
+    # Whether :js is true or not, this block will run when "type: :system"
+      driven_by :rack_test #:rack_test for faster execution, but it won't save pictures
+    # driven_by :selenium_chrome # Use :selenium_chrome to see the browser
+    # driven_by :selenium_chrome_headless # Use :rack_test for faster execution, but :selenium can save pictures
+    end
+=begin
+ config.before(:each, type: :system, js: true) do
+    # When 'js: true' is added, this will "also" run and override the driver choice
+    
+    driven_by :selenium_chrome_headless
+  end
+
+  Capybara.asset_host = 'http://localhost:3000' # For better looking HTML screenshots
+  # To use subdomain in the local environment, lvh.me is helpful to refer localhost easily
+  # Example:
+  #   Capybara.app_host = 'http://lvh.me'
+
+  Capybara::Screenshot.prune_strategy = :keep_last_run # Remove old files
+
+  # Devise
+  # For Devise Log-in - https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
+  config.include Warden::Test::Helpers
+
+  # To make sure Devise log-in works correctly, reset warden after each test
+  config.after(:each, type: :system) do
+    Warden.test_reset!
+  end
+
+=end
+  end
